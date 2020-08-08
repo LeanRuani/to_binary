65a7840762da70dc9e0734e28b7a8bc974017c22
Leandro <quenochetete1120@gmail.com>
2020-08-08 11:03:42 -0300

    Updates

---
+++ precache-manifest.28ee054532e3539187ecef0614b042d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ab45401747868e8a74b49516d55f3d",
+    "url": "/to_binary/index.html"
+  },
+  {
+    "revision": "02986f2ada5c3a1893ee",
+    "url": "/to_binary/static/css/main.16d4c7b9.chunk.css"
+  },
+  {
+    "revision": "451357e5483acea94b8f",
+    "url": "/to_binary/static/js/2.4045f2ed.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/to_binary/static/js/2.4045f2ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02986f2ada5c3a1893ee",
+    "url": "/to_binary/static/js/main.770a57ff.chunk.js"
+  },
+  {
+    "revision": "48b4add998ae95bb1666",
+    "url": "/to_binary/static/js/runtime-main.e4d74297.js"
+  }
+]);
